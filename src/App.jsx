ab5f8ac6432016084f719656256946ec5e6cd142
atxpaul <pablo.pan.veira@gmail.com>
2025-04-16 20:36:22 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Sysarmy!</h1>
+      <p>This is just a example website</p>
+    </div>
+  )
+}
+
+export default App
